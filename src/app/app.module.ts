2af9af23696a35270bc4346311f635293e7a81f4
Angot <bat76640@gmail.com>
2020-09-07 14:41:30 +0200

    Create menu and create config in app.module.ts to make some routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,13 @@ import { RaceComponent } from './race/race.component';
 import { PonyDetailComponent } from './pony-detail/pony-detail.component';
 import { RaceDetailComponent } from './race-detail/race-detail.component';
 import { NameRacePipePipe } from './name-race-pipe.pipe';
+import { MenuComponent } from './menu/menu.component';
+import {RouterModule, Routes} from "@angular/router";
 
+const ROUTES: Routes = [
+  { path: 'ponies', component: PoniesComponent},
+  { path: 'races', component: RaceComponent},
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,12 +21,15 @@ import { NameRacePipePipe } from './name-race-pipe.pipe';
     RaceComponent,
     PonyDetailComponent,
     RaceDetailComponent,
-    NameRacePipePipe
+    NameRacePipePipe,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
